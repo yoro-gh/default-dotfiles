c12740e628662d39f0852d19f98c7eee684e8a4b
yorogh <13429549+yoro-gh@users.noreply.github.com>
2021-12-12 00:18:22 +0800

    Initial collection from ubuntu, fedora and macOS.

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # default-dotfiles
+
+Collection of default dotfiles for easy reference
+(focus on bash/sh shells).
+
+## Ubuntu
+
+Ubuntu 20.04 from `multipass shell`.
+
+## Fedora
+
+Fedora core 35 from `podman machine ssh`.
+
+## macOS
+
+macOS 12.0.1 Monterey
+
+SSH configuration has changed layout from macOS 11 (Big Slur)'s single `sshd_config` file. Instead, `sshd_config` now sources `sshd_config.d/100-macos.conf` to enable password authentication.
---
+++ fedoracore35/etc/profile.d/console-login-helper-messages-profile.sh
@@ -0,0 +1,20 @@
+# /usr/share/console-login-helper-messages/profile.sh
+
+# Originally from https://github.com/coreos/baselayout/blob/master/baselayout/coreos-profile.sh
+
+# Only print for interactive shells.
+if [[ $- == *i* ]]; then
+  # If not using systemd, return.
+  if ! grep -q 'systemd' /proc/1/stat; then
+    return 0
+  fi
+
+  FAILED=$(systemctl list-units --state=failed --no-legend --plain)
+
+  if [[ ! -z "${FAILED}" ]]; then
+    COUNT=$(wc -l <<<"${FAILED}")
+    echo "[systemd]"
+    echo -e "Failed Units: \033[31m${COUNT}\033[39m"
+    awk '{ print "  " $1 }' <<<"${FAILED}"
+  fi
+fi
